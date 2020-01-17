294602e049c2fa9bbe0fd8c4cfccbdcbc17924f2
zengjiaozi <906712908@qq.com>
2020-01-17 15:29:04 +0800

    Initial commit

---
+++ app/src/main/java/com/collapsable/ScrollingActivity.java
@@ -0,0 +1,22 @@
+package com.collapsable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+
+
+}
